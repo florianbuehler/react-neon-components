a5c08720d8f37d462b2295a5ead7498d37ad3a2b
Florian Bühler <florian-buehler@outlook.com>
2021-11-07 13:04:29 +0100

    Move base style into separate file.

--- src/components/headers/h1/NeonH1.tsx
+++ src/components/headers/h1/NeonH1.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { NeonStyle } from '../../index.types';
-import { scaleSize } from '../../../utils/cssSize';
+import { baseStyle } from '../../shared/styles';
 
 const StyledH1 = styled.h1<NeonStyle>`
-  color: #fff;
-  font-weight: 400;
-  font-size: ${({ fontSize }: NeonStyle) => fontSize};
-  text-shadow: ${({ fontSize, color }: NeonStyle) =>
-    `0 0 7px #fff,
-     0 0 10px #fff, 
-     0 0 21px #fff, 
-     0 0 ${scaleSize(fontSize!, 0.63)} ${color}, 
-     0 0 ${scaleSize(fontSize!, 1.22)} ${color}, 
-     0 0 ${scaleSize(fontSize!, 1.37)} ${color}, 
-     0 0 ${scaleSize(fontSize!, 1.52)} ${color}, 
-     0 0 ${scaleSize(fontSize!, 2.25)} ${color};
-    `};
+  ${baseStyle};
 `;
 
 const NeonH1: React.FC<NeonStyle> = ({ children, ...neonStyle }): React.ReactElement => {
---
+++ src/components/shared/styles.ts
@@ -0,0 +1,19 @@
+﻿import { css } from 'styled-components';
+import { NeonStyle } from '../index.types';
+import { scaleSize } from '../../utils/cssSize';
+
+export const baseStyle = css<NeonStyle>`
+  color: #fff;
+  font-weight: 400;
+  font-size: ${({ fontSize }: NeonStyle) => fontSize};
+  text-shadow: ${({ fontSize, color }: NeonStyle) =>
+    `0 0 7px #fff,
+     0 0 10px #fff, 
+     0 0 21px #fff, 
+     0 0 ${scaleSize(fontSize!, 0.63)} ${color}, 
+     0 0 ${scaleSize(fontSize!, 1.22)} ${color}, 
+     0 0 ${scaleSize(fontSize!, 1.37)} ${color}, 
+     0 0 ${scaleSize(fontSize!, 1.52)} ${color}, 
+     0 0 ${scaleSize(fontSize!, 2.25)} ${color};
+    `};
+`;
