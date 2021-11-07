a33372be15f820dddf967f9153649ae9f2003881
Florian Bühler <florian-buehler@outlook.com>
2021-11-07 11:51:45 +0100

    Create skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    // see: https://stackoverflow.com/questions/63818415/react-was-used-before-it-was-defined
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  }
+};
---
+++ src/components/Text.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Text: React.FC = () => {
+  return <p>hello world</p>;
+};
+
+export default Text;
